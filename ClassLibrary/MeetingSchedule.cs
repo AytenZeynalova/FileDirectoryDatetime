f48f12c73d68588a2151b7126b2abbb8e756a59e
AytenZeynalova <14rvxpg@code.edu.az>
2022-04-13 11:32:02 +0400

    FileDirectoryDATETIME

---
+++ ClassLibrary/MeetingSchedule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+   public class MeetingSchedule
+    {
+        public List<Meeting> Meetings = new List<Meeting>();
+
+        public void SetMeeting(string fullname, DateTime from, DateTime to)
+        {
+           
+            
+                if(Meetings.Exists(x=>(x.FromDate>from && from<x.ToDate)||(x.ToDate<from && x.FromDate<from)))
+                {
+                    Meeting meeting = new Meeting();
+                    meeting.Name = fullname;
+                    meeting.FromDate = from;
+                    meeting.ToDate = to;
+
+                    Meetings.Add(meeting);
+                }
+                else
+                {
+                    throw new Exception("bu vaxt intervalinda artiq meeting vardir");
+                }
+            
+        }
+
+        public int FindMeetingsCount(DateTime datetime)
+        {
+            return Meetings.FindAll(x => x.FromDate > datetime).Count;
+        }
+
+        public bool IsExistsMeetingByName(string name)
+        {
+            return Meetings.Exists(x => x.Name == name);
+        }
+
+        public List<Meeting> GetExistMeetings(string name)
+        {
+            List<Meeting> ExistingMeeting = new List<Meeting>();
+
+            return Meetings.FindAll(x => x.Name.Contains(name));
+
+        }
+    }
+}
---
+++ FileDirectoryDatetime/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ClassLibrary;
+namespace FileDirectoryDatetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Meeting meeting1 = new Meeting();
+            meeting1.Name = "meeting1";
+            meeting1.FromDate = new DateTime(2022, 1, 10, 11, 24, 0);
+            meeting1.ToDate = new DateTime(2022, 1, 10, 15, 0, 0);
+
+            Meeting meeting2 = new Meeting();
+            meeting2.Name = "meeting1";
+            meeting2.FromDate = new DateTime(2022,1 , 10, 7, 24, 0);
+            meeting2.ToDate = new DateTime(2022, 1, 10, 10, 0, 0);
+
+            MeetingSchedule meetingSchedule = new MeetingSchedule();
+            meetingSchedule.Meetings.Add(meeting1);
+            meetingSchedule.Meetings.Add(meeting2);
+
+
+            DateTime from = new DateTime(2022, 1, 10,8 , 25, 0);
+            DateTime to = new DateTime(2022, 1, 10, 11, 0, 0);
+            meetingSchedule.SetMeeting("meeting3", from, to);
+
+
+            Console.WriteLine(";;;;;;;;;;;;;;;;;;;All Meetings;;;;;;;;;;;;;;;;;;\n");
+            foreach (var item in meetingSchedule.Meetings)
+            {
+                Console.WriteLine(item.Name + "\n");
+            }
+
+            Console.WriteLine(";;;;;;;;;;;;;;;;;;Find Meetings Count;;;;;;;;;;;;;;;;;;\n");
+            Console.WriteLine(meetingSchedule.FindMeetingsCount(new DateTime(2022, 4, 13, 11, 30, 0)) + "\n");
+
+
+            Console.WriteLine(";;;;;;;;;;;;;;;;;;Is Exists Meeting By Name;;;;;;;;;;;;;;;;;\n");
+            Console.WriteLine(meetingSchedule.IsExistsMeetingByName("meeting4"));
+
+
+            Console.WriteLine(";;;;;;;;;;;;;;;;Get Exist Meetings;;;;;;;;;;;;;\n");
+            foreach (var item in meetingSchedule.GetExistMeetings("meeting3"))
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
